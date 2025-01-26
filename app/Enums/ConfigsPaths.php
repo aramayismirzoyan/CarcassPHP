39e3474a7f826336b38edaa4cf86fdb9cbf118e4
Aramayis <aramayismirzoyan@gmail.com>
2025-01-27 02:20:01 +0400

    [feat] adding enum for configs paths

---
+++ app/Enums/ConfigsPaths.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums;
+
+enum ConfigsPaths
+{
+    case DB;
+    case ROUTES;
+
+    public function get(): string
+    {
+        $rootPath = dirname(dirname(__DIR__));
+        return match ($this) {
+            self::DB => $rootPath . DIRECTORY_SEPARATOR. 'config' . DIRECTORY_SEPARATOR . 'db.php',
+            self::ROUTES => $rootPath . DIRECTORY_SEPARATOR. 'config' . DIRECTORY_SEPARATOR.  'routes.php',
+            default => ''
+        };
+    }
+
+}
